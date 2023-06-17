afa85aafff176865092c18a539cb6fd3fe66dd0b
N8Brooks <natebrooks004@gmail.com>
2023-06-16 19:48:51 -0500

    feat: setup py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""Setup script"""
+
+from setuptools import setup
+
+setup(
+    name="bit_set",
+    description="Python implementation of the BitSet class.",
+    author="Nathan Brooks",
+    author_email="natebrooks004@gmail.com",
+    url="https://github.com/N8Brooks/bit_set",
+)
